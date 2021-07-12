84d1c7690d25293f88608eb065ac0321e22363b4
María Imbert <imbert.maria@hotmail.com>
2021-07-12 20:19:41 -0300

    database

--- database/db.sql
+++ database/db.sql
@@ -14,3 +14,8 @@ create table contacto (
 
 describe contacto;
 
+insert into contacto values (null, 'NombreCont1', 'pruebacontacto1@gmail.com', 1234567, 'Consulta1', 'clases1');
+insert into contacto values (null, 'NombreCont2', 'pruebacontacto2@gmail.com', 1234568, 'Consulta2', 'clases2');
+insert into contacto values (null, 'NombreCont3', 'pruebacontacto3@gmail.com', 1234569, 'Consulta3', 'clases3');
+
+select * from contacto;
